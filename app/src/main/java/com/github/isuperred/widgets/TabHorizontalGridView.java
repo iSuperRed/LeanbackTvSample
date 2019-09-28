618e9be70ae9f9ffb7ed25d02a31cb106f866978
iSuperRed <673693797@qq.com>
2019-09-28 15:18:26 +0800

    焦点抖动

--- app/src/main/java/com/github/isuperred/widgets/TabHorizontalGridView.java
+++ app/src/main/java/com/github/isuperred/widgets/TabHorizontalGridView.java
@@ -2,11 +2,14 @@ package com.github.isuperred.widgets;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.FocusFinder;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 
 import androidx.leanback.widget.HorizontalGridView;
 
+import com.github.isuperred.R;
 import com.github.isuperred.utils.Constants;
 
 /**
@@ -26,6 +29,23 @@ public class TabHorizontalGridView extends HorizontalGridView {
 
     public TabHorizontalGridView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+    }
+
+    @Override
+    public View focusSearch(View focused, int direction) {
+        if (focused != null) {
+            if (direction == View.FOCUS_LEFT) {
+                final FocusFinder ff = FocusFinder.getInstance();
+                final View found = ff.findNextFocus(this, focused, direction);
+                if (found == null || found.getId() != R.id.tv_main_title) {
+                    focused.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.host_shake));
+                    return null;
+                }
+
+            }
+
+        }
+        return super.focusSearch(focused, direction);
     }
 
     @Override
