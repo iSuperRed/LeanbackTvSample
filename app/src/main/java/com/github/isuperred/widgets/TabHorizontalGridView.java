888eaec53a6f1b75a1b6e8670f0904bf7950d8f2
iSuperRed <673693797@qq.com>
2019-10-26 23:26:00 +0800

    删除无用代码

--- app/src/main/java/com/github/isuperred/widgets/TabHorizontalGridView.java
+++ app/src/main/java/com/github/isuperred/widgets/TabHorizontalGridView.java
@@ -2,7 +2,6 @@ package com.github.isuperred.widgets;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.FocusFinder;
 import android.view.KeyEvent;
 import android.view.View;
