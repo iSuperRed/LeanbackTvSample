fee9b141a1a2c3b7740b216a5c3bf90f094b86b4
iSuperRed <673693797@qq.com>
2019-11-02 18:45:31 +0800

    适配8.0

--- app/src/main/java/com/github/isuperred/presenter/AppInstalledPresenter.java
+++ app/src/main/java/com/github/isuperred/presenter/AppInstalledPresenter.java
@@ -2,22 +2,20 @@ package com.github.isuperred.presenter;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 import androidx.leanback.widget.Presenter;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.github.isuperred.R;
 import com.github.isuperred.bean.AppInfo;
 
@@ -46,8 +44,7 @@ public class AppInstalledPresenter extends Presenter {
         if (item instanceof AppInfo) {
             ViewHolder vh = (ViewHolder) viewHolder;
             if (((AppInfo) item).icon != null) {
-                BitmapDrawable btDrawable = (BitmapDrawable) ((AppInfo) item).icon;
-                Bitmap bitmap = btDrawable.getBitmap();
+                Bitmap bitmap = getBitmapFromDrawable(((AppInfo) item).icon);//适配Android 8.0
                 RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory.create(mContext.getResources(), bitmap);
                 drawable.setCornerRadius(mContext.getResources().getDimension(R.dimen.px20));
                 vh.mIvAppIcon.setImageDrawable(drawable);
@@ -73,5 +70,14 @@ public class AppInstalledPresenter extends Presenter {
             mIvAppIcon = view.findViewById(R.id.iv_app_icon);
             mTvAppName = view.findViewById(R.id.tv_app_name);
         }
+    }
+
+    private Bitmap getBitmapFromDrawable(@NonNull Drawable drawable) {
+        final Bitmap bmp = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(bmp);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bmp;
     }
 }
